b4b364e6a29328c15a45e191ff53718b8f45ee84
mariana-leite92 <leite.mari92@gmail.com>
2021-04-15 10:02:35 -0300

    Novas alteracoes

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     HOST: "localhost",
-    USER: "admin",
-    PASSWORD: "140792M@ri",
+    USER: "root",
+    PASSWORD: "...",
     DB: "db_biblioteca",
     dialect: "mysql",
     pool: {
